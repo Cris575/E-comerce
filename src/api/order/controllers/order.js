33a57f1d6a330087597acb02618fc6c95fddc90a
Cristian Gonzalez Herrera <cg691539@gmail.com>
2024-06-03 20:25:39 -0600

    commit

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -1,9 +1,18 @@
-'use strict';
+"use strict";
+
+// @ts-ignore
+const stripe = require("stripe")(
+  "sk_test_51PIEmLIODRq79970BemRPxgmcBQaerZyKEdoWzZYAT0OxaY812kNdCSuubVExIx7oXe2RHOmmOg6rwpJ0dmqGcQD00cyRlBrCC",
+);
 
 /**
  * order controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::order.order');
+module.exports = createCoreController("api::order.order", ({ strapi }) => ({
+  async paymentOrder(ctx) {
+    console.log("OK");
+  },
+}));
